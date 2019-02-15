552749ec099851598f99d0ecbbbe282541dbc317
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2019-02-15 20:08:26 +0330

    use `pick` function to pick object properties

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const provinces = require('./data/provinces');
 const cities = require('./data/cities');
+const pick = require('./lib/pick');
 
 exports.getProvinces = function () {
 	return provinces
-		.map(({id, name}) => ({id, name}));
+		.map(p => pick(p, ['id', 'name']));
 };
 
 exports.getCities = function (provinceID) {
 	return cities
 		.filter(c => c.province === provinceID)
-		.map(({id, name, province}) => ({id, name, province}));
+		.map(c => pick(c, ['id', 'name', 'province']));
 };
-
